df77498198fdf6e53796abe2c78bcb174abc1a94
Matthew Weir <matthew.weir@mcvts.org>
2021-11-18 09:07:13 -0500

    Separate file for global functions and level files.

--- code/editor.ts
+++ code/editor.ts
@@ -1,6 +1,6 @@
 import "bootstrap";
-import kaboom, {AreaComp, CircleComp, ColorComp, GameObj, OriginComp, PosComp, SpriteComp} from "kaboom";
-import Global from "./global";
+import kaboom, {AreaComp, CircleComp, ColorComp, GameObj, OriginComp, PosComp} from "kaboom";
+import Global, {Level, LevelInitInfo} from "./global";
 
 const kbm = kaboom({
     width: 1000,
@@ -20,27 +20,6 @@ let mouse = vec2();
 let editing = true;
 
 Global.init(mouse);
-
-type WallInitInfo = {
-    x: number,
-    y: number,
-    dir: "vertical" | "horizontal",
-    length: number
-}
-
-type LevelInitInfo = {
-    playerX: number,
-    playerY: number,
-    blockX: number,
-    blockY: number,
-    goalX: number,
-    goalY: number,
-    walls: WallInitInfo[],
-    switchWalls: WallInitInfo[],
-    switchX?: number,
-    switchY?: number,
-    size: number
-}
 
 let currentLevelData: LevelInitInfo = {
     playerX: 0,
@@ -69,199 +48,9 @@ function transition() {
 }
 
 scene("test", () => {
-    const cellSize = 1000 / currentLevelData.size;
-    let switchObj;
-    let hasWon = false;
-    let moving = false;
-
-    // Add grid
-    for (let i = 0; i < currentLevelData.size; i++) {
-        for (let j = 0; j < currentLevelData.size; j++) {
-            let grid: (SpriteComp|PosComp|ColorComp|string)[] = [
-                sprite(`grid`, {width: cellSize, height: cellSize, quad: quad(i % 4 * 0.25, j % 4 * 0.25, 0.25, 0.25)}),
-                pos(i * cellSize, j * cellSize),
-                "grid"
-            ];
-            if (i >= currentLevelData.goalX && i <= currentLevelData.goalX + 4 && j >= currentLevelData.goalY && j <= currentLevelData.goalY + 4) grid.push(color(100, 100, 100));
-            add(grid);
-        }
-    }
-
-    // Add edges
-    add([
-        pos(0, 0),
-        area({width: 1, height: 1000}),
-        solid()
-    ]);
-    add([
-        pos(999, 0),
-        area({width: 1, height: 1000}),
-        solid()
-    ]);
-    add([
-        pos(0, 0),
-        area({width: 1000, height: 1}),
-        solid()
-    ]);
-    add([
-        pos(0, 999),
-        area({width: 1000, height: 1}),
-        solid()
-    ]);
-
-    // Add goal
-    const goal = add([
-        pos(currentLevelData.goalX * cellSize, currentLevelData.goalY * cellSize),
-        area({width: cellSize * 5, height: cellSize * 5}),
-        "goal"
-    ]);
-
-    // Add switch
-    if (typeof currentLevelData.switchX === "number" && typeof currentLevelData.switchY === "number") {
-        switchObj = add([
-            sprite("switch", {width: cellSize, height: cellSize}),
-            pos(currentLevelData.switchX * cellSize, currentLevelData.switchY * cellSize),
-            area({width: cellSize, height: cellSize})
-        ])
-    }
-
-    // Add block
-    const block = add([
-        sprite("block", {width: cellSize * 5, height: cellSize * 5}),
-        area({width: cellSize * 5 - 2, height: cellSize * 5 - 2}),
-        solid(),
-        pos(currentLevelData.blockX * cellSize + 1, currentLevelData.blockY * cellSize + 1),
-        "block"
-    ]);
-
-    // Add player
-    const player = add([
-        sprite("player", {width: cellSize, height: cellSize, }),
-        area({width: cellSize - 2, height: cellSize - 2}),
-        solid(),
-        pos(currentLevelData.playerX * cellSize + 1, currentLevelData.playerY * cellSize + 1),
-        "player"
-    ]);
-
-    // Add walls
-    for (let i = 0; i < currentLevelData.walls.length; i++) {
-        let wall = currentLevelData.walls[i];
-        add([
-            pos(wall.dir === "vertical" ? wall.x * cellSize - 1 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 1),
-            area({width: wall.dir === "vertical" ? 2 : wall.length * cellSize, height: wall.dir === "vertical" ? wall.length * cellSize : 2}),
-            solid()
-        ]);
-        add([
-            rect(wall.dir === "vertical" ? 6 : wall.length * cellSize, wall.dir === "vertical" ? wall.length * cellSize : 6),
-            pos(wall.dir === "vertical" ? wall.x * cellSize - 3 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 3),
-            color(0, 255, 0)
-        ])
-    }
-    for (let i = 0; i < currentLevelData.switchWalls.length; i++) {
-        let wall = currentLevelData.switchWalls[i];
-        add([
-            pos(wall.dir === "vertical" ? wall.x * cellSize - 1 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 1),
-            area({width: wall.dir === "vertical" ? 2 : wall.length * cellSize, height: wall.dir === "vertical" ? wall.length * cellSize : 2}),
-            solid(),
-            color(0, 0, 255),
-            "switchWallCollision"
-        ]);
-        add([
-            rect(wall.dir === "vertical" ? 6 : wall.length * cellSize, wall.dir === "vertical" ? wall.length * cellSize : 6),
-            pos(wall.dir === "vertical" ? wall.x * cellSize - 3 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 3),
-            color(0, 0, 255),
-            opacity(1),
-            "switchWallModel"
-        ]);
-    }
-
-    // Directional movement
-    keyPress(["left", "a"], async () => {
-        if (hasWon || moving) return;
-        moving = true;
-        let sound = play("move");
-        for (let i = 0; i < 4; i++) {
-            if (player.pos.x === block.pos.x + cellSize * 5 && player.pos.y === block.pos.y + cellSize * 2 ) block.moveBy(cellSize / -4, 0);
-            player.moveBy(cellSize / -4, 0);
-            await wait(0);
-        }
-        player.pos.x++;
-        if (player.pos.x % cellSize !== 1) player.pos.x = player.pos.x - player.pos.x % cellSize + 1;
-        sound.stop();
-        moving = false;
+    new Level(currentLevelData, transition, function() {
+        if (!this.hasWon) go("test");
     });
-
-    keyPress(["right", "d"], async () => {
-        if (hasWon || moving) return;
-        moving = true;
-        let sound = play("move");
-        for (let i = 0; i < 4; i++) {
-            if (player.pos.x === block.pos.x - cellSize && player.pos.y === block.pos.y + cellSize * 2) block.moveBy(cellSize / 4, 0);
-            player.moveBy(cellSize / 4, 0);
-            await wait(0);
-        }
-        if (player.pos.x % cellSize !== 1) player.pos.x = player.pos.x - player.pos.x % cellSize + 1;
-        sound.stop();
-        moving = false;
-    });
-
-    keyPress(["up", "w"], async () => {
-        if (hasWon || moving) return;
-        moving = true;
-        let sound = play("move");
-        for (let i = 0; i < 4; i++) {
-            if (player.pos.x === block.pos.x + cellSize * 2 && player.pos.y === block.pos.y + cellSize * 5) block.moveBy(0, cellSize / -4);
-            player.moveBy(0, cellSize / -4);
-            await wait(0);
-        }
-        player.pos.y++;
-        if (player.pos.y % cellSize !== 1) player.pos.y = player.pos.y - player.pos.y % cellSize + 1;
-        sound.stop();
-        moving = false;
-    });
-
-    keyPress(["down", "s"], async () => {
-        if (hasWon || moving) return;
-        moving = true;
-        let sound = play("move");
-        for (let i = 0; i < 4; i++) {
-            if (player.pos.x === block.pos.x + cellSize * 2 && player.pos.y === block.pos.y - cellSize) block.moveBy(0, cellSize / 4);
-            player.moveBy(0, cellSize / 4);
-            await wait(0);
-        }
-        if (player.pos.y % cellSize !== 1) player.pos.y = player.pos.y - player.pos.y % cellSize + 1;
-        sound.stop();
-        moving = false;
-    });
-
-    // Reset on "R"
-    keyPress("r", async () => {
-        if (hasWon) return;
-        go("test");
-    });
-
-    if (switchObj) switchObj.collides("block", () => {
-        play("switch");
-    })
-
-    action(async () => {
-        if (goal.pos.x === block.pos.x - 1 && goal.pos.y === block.pos.y - 1 && !hasWon) {
-            hasWon = true;
-            every("grid", i => i.color = {r: 255, g: 255, b: 0});
-            play("finish");
-            await wait(3);
-            transition();
-        }
-        if (switchObj) {
-            if (block.isTouching(switchObj)) {
-                every("switchWallCollision", i => i.solid = false);
-                every("switchWallModel", i => i.opacity = 0.5);
-            } else {
-                every("switchWallCollision", i => i.solid = true);
-                every("switchWallModel", i => i.opacity = 1);
-            }
-        }
-    })
 });
 
 let addingWall = false;
--- code/global.ts
+++ code/global.ts
@@ -1,6 +1,6 @@
-import {Vec2} from "kaboom";
+import {ColorComp, PosComp, SpriteComp, Vec2} from "kaboom";
 
-class Global {
+export default class Global {
     static init(mouse: Vec2) {
         addEventListener("mousemove", e => {
             mouse.x = Math.floor((e.x - canvas.getBoundingClientRect().x) / canvas.getBoundingClientRect().width * 1000);
@@ -19,4 +19,223 @@ class Global {
     }
 }
 
-export default Global;
+export class Level {
+    hasWon: boolean;
+
+    constructor(options: LevelInitInfo, onComplete: () => void, onReset: () => void) {
+        const cellSize = 1000 / options.size;
+        let switchObj;
+        this.hasWon = false;
+        let moving = false;
+
+        // Add grid
+        for (let i = 0; i < options.size; i++) {
+            for (let j = 0; j < options.size; j++) {
+                let grid: (SpriteComp|PosComp|ColorComp|string)[] = [
+                    sprite(`grid`, {width: cellSize, height: cellSize, quad: quad(i % 4 * 0.25, j % 4 * 0.25, 0.25, 0.25)}),
+                    pos(i * cellSize, j * cellSize),
+                    "grid"
+                ];
+                if (i >= options.goalX && i <= options.goalX + 4 && j >= options.goalY && j <= options.goalY + 4) grid.push(color(100, 100, 100));
+                add(grid);
+            }
+        }
+
+        // Add edges
+        add([
+            pos(0, 0),
+            area({width: 1, height: 1000}),
+            solid()
+        ]);
+        add([
+            pos(999, 0),
+            area({width: 1, height: 1000}),
+            solid()
+        ]);
+        add([
+            pos(0, 0),
+            area({width: 1000, height: 1}),
+            solid()
+        ]);
+        add([
+            pos(0, 999),
+            area({width: 1000, height: 1}),
+            solid()
+        ]);
+
+        // Add goal
+        const goal = add([
+            pos(options.goalX * cellSize, options.goalY * cellSize),
+            area({width: cellSize * 5, height: cellSize * 5}),
+            "goal"
+        ]);
+
+        // Add switch
+        if (typeof options.switchX === "number" && typeof options.switchY === "number") {
+            switchObj = add([
+                sprite("switch", {width: cellSize, height: cellSize}),
+                pos(options.switchX * cellSize, options.switchY * cellSize),
+                area({width: cellSize, height: cellSize})
+            ])
+        }
+
+        // Add block
+        const block = add([
+            sprite("block", {width: cellSize * 5, height: cellSize * 5}),
+            area({width: cellSize * 5 - 2, height: cellSize * 5 - 2}),
+            solid(),
+            pos(options.blockX * cellSize + 1, options.blockY * cellSize + 1),
+            "block"
+        ]);
+
+        // Add player
+        const player = add([
+            sprite("player", {width: cellSize, height: cellSize, }),
+            area({width: cellSize - 2, height: cellSize - 2}),
+            solid(),
+            pos(options.playerX * cellSize + 1, options.playerY * cellSize + 1),
+            "player"
+        ]);
+
+        // Add walls
+        for (let i = 0; i < options.walls.length; i++) {
+            let wall = options.walls[i];
+            add([
+                pos(wall.dir === "vertical" ? wall.x * cellSize - 1 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 1),
+                area({width: wall.dir === "vertical" ? 2 : wall.length * cellSize, height: wall.dir === "vertical" ? wall.length * cellSize : 2}),
+                solid()
+            ]);
+            add([
+                rect(wall.dir === "vertical" ? 6 : wall.length * cellSize, wall.dir === "vertical" ? wall.length * cellSize : 6),
+                pos(wall.dir === "vertical" ? wall.x * cellSize - 3 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 3),
+                color(0, 255, 0)
+            ])
+        }
+        for (let i = 0; i < options.switchWalls.length; i++) {
+            let wall = options.switchWalls[i];
+            add([
+                pos(wall.dir === "vertical" ? wall.x * cellSize - 1 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 1),
+                area({width: wall.dir === "vertical" ? 2 : wall.length * cellSize, height: wall.dir === "vertical" ? wall.length * cellSize : 2}),
+                solid(),
+                color(0, 0, 255),
+                "switchWallCollision"
+            ]);
+            add([
+                rect(wall.dir === "vertical" ? 6 : wall.length * cellSize, wall.dir === "vertical" ? wall.length * cellSize : 6),
+                pos(wall.dir === "vertical" ? wall.x * cellSize - 3 : wall.x * cellSize, wall.dir === "vertical" ? wall.y * cellSize : wall.y * cellSize - 3),
+                color(0, 0, 255),
+                opacity(1),
+                "switchWallModel"
+            ]);
+        }
+
+        // Directional movement
+        keyPress(["left", "a"], async () => {
+            if (this.hasWon || moving) return;
+            moving = true;
+            let sound = play("move");
+            for (let i = 0; i < 4; i++) {
+                if (player.pos.x === block.pos.x + cellSize * 5 && player.pos.y === block.pos.y + cellSize * 2 ) block.moveBy(cellSize / -4, 0);
+                player.moveBy(cellSize / -4, 0);
+                await wait(0);
+            }
+            player.pos.x++;
+            if (player.pos.x % cellSize !== 1) player.pos.x = player.pos.x - player.pos.x % cellSize + 1;
+            sound.stop();
+            moving = false;
+        });
+
+        keyPress(["right", "d"], async () => {
+            if (this.hasWon || moving) return;
+            moving = true;
+            let sound = play("move");
+            for (let i = 0; i < 4; i++) {
+                if (player.pos.x === block.pos.x - cellSize && player.pos.y === block.pos.y + cellSize * 2) block.moveBy(cellSize / 4, 0);
+                player.moveBy(cellSize / 4, 0);
+                await wait(0);
+            }
+            if (player.pos.x % cellSize !== 1) player.pos.x = player.pos.x - player.pos.x % cellSize + 1;
+            sound.stop();
+            moving = false;
+        });
+
+        keyPress(["up", "w"], async () => {
+            if (this.hasWon || moving) return;
+            moving = true;
+            let sound = play("move");
+            for (let i = 0; i < 4; i++) {
+                if (player.pos.x === block.pos.x + cellSize * 2 && player.pos.y === block.pos.y + cellSize * 5) block.moveBy(0, cellSize / -4);
+                player.moveBy(0, cellSize / -4);
+                await wait(0);
+            }
+            player.pos.y++;
+            if (player.pos.y % cellSize !== 1) player.pos.y = player.pos.y - player.pos.y % cellSize + 1;
+            sound.stop();
+            moving = false;
+        });
+
+        keyPress(["down", "s"], async () => {
+            if (this.hasWon || moving) return;
+            moving = true;
+            let sound = play("move");
+            for (let i = 0; i < 4; i++) {
+                if (player.pos.x === block.pos.x + cellSize * 2 && player.pos.y === block.pos.y - cellSize) block.moveBy(0, cellSize / 4);
+                player.moveBy(0, cellSize / 4);
+                await wait(0);
+            }
+            if (player.pos.y % cellSize !== 1) player.pos.y = player.pos.y - player.pos.y % cellSize + 1;
+            sound.stop();
+            moving = false;
+        });
+
+        // Reset on "R"
+        keyPress("r", async () => {
+            onReset();
+        });
+
+        if (switchObj) switchObj.collides("block", () => {
+            play("switch");
+        })
+
+        action(async () => {
+            if (goal.pos.x === block.pos.x - 1 && goal.pos.y === block.pos.y - 1 && !this.hasWon) {
+                this.hasWon = true;
+                every("grid", i => i.color = {r: 255, g: 255, b: 0});
+                play("finish");
+                await wait(3);
+                onComplete();
+                this.hasWon = false;
+            }
+            if (switchObj) {
+                if (block.isTouching(switchObj)) {
+                    every("switchWallCollision", i => i.solid = false);
+                    every("switchWallModel", i => i.opacity = 0.5);
+                } else {
+                    every("switchWallCollision", i => i.solid = true);
+                    every("switchWallModel", i => i.opacity = 1);
+                }
+            }
+        })
+    }
+}
+
+export type WallInitInfo = {
+    x: number,
+    y: number,
+    dir: string,
+    length: number
+}
+
+export type LevelInitInfo = {
+    playerX: number,
+    playerY: number,
+    blockX: number,
+    blockY: number,
+    goalX: number,
+    goalY: number,
+    walls: WallInitInfo[],
+    switchWalls: WallInitInfo[],
+    switchX?: number,
+    switchY?: number,
+    size: number
+}
